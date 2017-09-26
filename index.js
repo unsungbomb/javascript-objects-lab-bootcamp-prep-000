6f3a0798d8199dafe3cf30af77bec5b5706d6e15
Sonny McPherson <unsungbomb@gmail.com>
2017-09-26 17:28:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var recipes = {}
 
-function updateObjectWithKeyAndValue(object, key, value){
-  return Object.assign({}, object, {[key]: value});
+function updateObjectWithKeyAndValue(object, key, value) {
+return Object.assign({}, object, {[key]: value});
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-object[key] = value;
-return object;
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
 }
 
 function deleteFromObjectByKey(object, key) {
@@ -18,4 +18,4 @@ function deleteFromObjectByKey(object, key) {
 function destructivelyDeleteFromObjectByKey(object, key) {
 delete object[key];
 return object;
-
+}
